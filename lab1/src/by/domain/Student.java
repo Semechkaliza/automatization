a2d32b095cb0d3eee23f2f15e00712e29c49ace9
Lizaveta Grishkina <grishkinaelizaveta@gmail.com>
2018-09-24 23:17:34 +0300

    refactor student class

--- lab1/src/by/domain/Student.java
+++ lab1/src/by/domain/Student.java
@@ -7,9 +7,9 @@ import java.util.List;
 @Data
 public class Student {
     private String name;
-    private Double avgMark;
     private List<Integer> marks;
-
+    private Double avgMark;
+    
     public Double calculateAvgMark(){
         Integer count=this.getMarks().size();
         Double mark=0d;
